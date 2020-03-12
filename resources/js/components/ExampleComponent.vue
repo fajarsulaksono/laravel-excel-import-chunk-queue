638d3558ae55d8cdeaf6abb13a3bf63d1067be25
Fajar Sulaksono <fajar.sulaksono@gmail.com>
2020-03-12 16:23:13 +0700

    update preset vue

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,20 @@
+<template>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                messages: [],
+                message: '',
+            }
+        },
+        mounted() {
+            Echo.channel('messages')
+                .listen('.newMessage', (message) => {
+                    this.messages.push(message);
+                    console.log(message);
+                });
+        }
+    }
+</script>
